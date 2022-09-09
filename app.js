9eff58a2e3d95488ccad7a36aefc1405d8f92c0c
jagatjit barik <itsjagatjitbarik@gmail.com>
2022-09-09 14:19:34 +0530

    second fhase completed

---
+++ app.js
@@ -0,0 +1,46 @@
+const sections = document.querySelectorAll(".section");
+const btnParent = document.querySelectorAll(".controlls");
+const btnChild = document.querySelectorAll(".control");
+const main = document.querySelector(".main-content");
+
+function pageTransition() {
+  //on button click add active class
+  for (let i = 0; i < btnChild.length; i++) {
+    btnChild[i].addEventListener("click", function () {
+      let currentBtn = document.querySelectorAll(".active-btn");
+
+      currentBtn[0].className = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
+
+      this.className += " active-btn";
+    });
+  }
+
+  // sections active on click
+  main.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+
+    if (id) {
+      // remove selected btn and add to new
+
+      btnParent.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+
+      e.target.classList.add("active");
+
+      // ? hide other sections
+
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
+}
+
+pageTransition();
